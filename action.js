0ec60176b56e1c7ec385f3ed70591dff8421e816
Gaby <gabyzarzamora@gmail.com>
2017-10-23 20:25:28 -0500

    PHP app enabled

--- action.js
+++ action.js
@@ -1,50 +1,72 @@
-function arreglo(){
-	var carros = [
-	["polo.jpg", "VW", "Polo", "Azul", "Dos puertas"], 
-	["charger.jpg", "Dodge", "Charger", "Rojo", "Cuatro puertas"],
-	["500.jpg", "Fiat", "500", "Blanco", "Dos puertas"]
-	];
-
-	var content="";
-	var carro="";
-
-	console.log(carros);
-
-	for(var i=0; i<carros.length; i++){
-		
-		carro = carro + "<div class='coche'>";
-		
-		for (var j=0; j<carros[i].length; j++){
-			if(j == 0){
-				console.log("Foto:" + carros[i][j]);
-				carro = carro + "<div class='foto'>" + carros[i][j] + "</div>";
-			} else {
-				if( j == 1){
-					console.log("Marca: " + carros[i][j]);
-					carro = carro + "<div class='marca'>" + carros[i][j] + "</div>"; 
-				} else {
-					if(j == 2){
-						console.log("Modelo: " + carros [i][j]);
-						carro = carro + "<div class='modelo'>" + carros[i][j] + "</div>";
-					} else{
-						if(j == 3){
-							console.log("Color" + carros[i][j]);
-							carro = carro + "<div class='color'>" + carros[i][j] + "</div>";
-						}else{
-							console.log("Puertas: " + carros[i][j]);
-							carro = carro + "<div class='puertas'>" + carros[i][j] + "</div>";
-						}
-					}
-				}
-			}
-		}
+function pokedex(){
+ var pokemones = [
+   {
+     "id": "001",
+     "name":"Pikachu",
+     "photo":"01",
+     "type": "electric"
+   },
+   {
+     "id": "002",
+     "name":"Charmander",
+     "photo":"02",
+     "type": "dragon"
+   },
+   {
+     "id": "003",
+     "name":"Squirtle",
+     "photo":"03",
+     "type": "fairy"
+   },
+   {
+     "id": "004",
+     "name":"Bulbasaur",
+     "photo":"04",
+     "type": "bug"
+   }
+ ];
+var id = "";
+var name = "";
+var photo = "";
+var type = "";
 
 
-		carro = carro + "</div>"
-		content = content + carro;
-		console.log(content);
-				carro=""
-	}
 
-	document.getElementById('lista').innerHTML = content;		
+var contentPhoto = "";
+
+
+var contentInfo = "";
+var infoName = "";
+var infoId = "";
+var infoType = "";
+var auxDiv = "";
+
+var content ="";
+
+
+for (var i = 0; i < pokemones.length; i++) {
+ id = pokemones[i].id;
+ name = pokemones[i].name;
+ photo = pokemones[i].photo;
+ type = pokemones[i].type;
+
+ console.log(id + name + photo + type);
+
+ contentPhoto = "<div class='photo'>" + "<img src='img/photos/" + photo + ".jpg'>" + "</div>";
+ 
+ infoName = "<h2>" + name  + "</h2>";
+ infoId = "<span>" + id + "</span>";
+ infoType = "<img src='img/types/" + type + ".png'>";
+ auxDiv = "<div class='d-flex justify-content-between'>" + infoId + infoType + "</div>";
+
+ contentInfo= "<div class='info'>" + infoName + auxDiv + "</div>";
+
+ content = content + "<div class='pokemon'>" + contentPhoto + contentInfo + "</div>";
+
+
+
+ }
+
+ document.getElementById('container').innerHTML = content;
+
 }
